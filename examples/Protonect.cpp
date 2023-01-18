776bebb3b15e0fa7459ae9ae5a0f921d9d5b7999
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2023-01-18 15:26:25 +0100

    many many pictures

--- examples/Protonect.cpp
+++ examples/Protonect.cpp
@@ -1,411 +1,287 @@
 /*
- * This file is part of the OpenKinect Project. http://www.openkinect.org
- *
- * Copyright (c) 2011 individual OpenKinect contributors. See the CONTRIB file
- * for details.
- *
- * This code is licensed to you under the terms of the Apache License, version
- * 2.0, or, at your option, the terms of the GNU General Public License,
- * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
- * or the following URLs:
- * http://www.apache.org/licenses/LICENSE-2.0
- * http://www.gnu.org/licenses/gpl-2.0.txt
- *
- * If you redistribute this file in source form, modified or unmodified, you
- * may:
- *   1) Leave this header intact and distribute it under the same terms,
- *      accompanying it with the APACHE20 and GPL20 files, or
- *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
- *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
- * In all cases you must keep the copyright notice intact and include a copy
- * of the CONTRIB file.
- *
- * Binary distributions must follow the binary distribution requirements of
- * either License.
- */
+-- Georgia Tech 2016 Spring
+--
+-- This is a sample code to show how to use the libfreenet2 with OpenCV
+--
+-- The code will streams RGB, IR and Depth images from an Kinect sensor.
+-- To use multiple Kinect sensor, simply initial other "listener" and "frames"
 
-/** @file Protonect.cpp Main application file. */
+-- This code refered from sample code provided from libfreenet2: Protonect.cpp
+-- https://github.com/OpenKinect/libfreenect2
+-- and another discussion from: http://answers.opencv.org/question/76468/opencvkinect-onekinect-for-windows-v2linuxlibfreenect2/
 
+
+-- Contact: Chih-Yao Ma at <cyma@gatech.edu>
+
+--Build option
+g++ KinectOneStream.cpp -std=c++11 -o out `pkg-config opencv --cflags --libs` `pkg-config freenect2 --cflags --libs` 
+
+*/
+
+//! [headers]
 #include <iostream>
-#include <cstdlib>
+#include <stdio.h>
+#include <iomanip>
+#include <time.h>
 #include <signal.h>
+#include <opencv2/opencv.hpp>
 
-/// [headers]
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/frame_listener_impl.h>
 #include <libfreenect2/registration.h>
 #include <libfreenect2/packet_pipeline.h>
 #include <libfreenect2/logger.h>
-/// [headers]
-#ifdef EXAMPLES_WITH_OPENGL_SUPPORT
-#include "viewer.h"
-#endif
+//! [headers]
+//
+#include "nanodet.h"
+#include <thread>         // std::thread
+#include <mutex>          // std::mutex, std::lock
 
+using namespace std;
+using namespace cv;
 
-bool protonect_shutdown = false; ///< Whether the running application should shut down.
+//! [context]
+libfreenect2::Freenect2 freenect2;
+libfreenect2::Freenect2Device *dev = 0;
+libfreenect2::PacketPipeline *pipeline = 0;
+//! [context]
+
+//! [listeners]
+libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color |
+                                              libfreenect2::Frame::Depth |
+                                              libfreenect2::Frame::Ir);
+libfreenect2::FrameMap frames;
+libfreenect2::Registration* registration;
+
+bool protonect_shutdown = false; // Whether the running application should shut down.
+
+cv::Mat rgbmat, depthmat, depthmatUndistorted, irmat, rgbd, rgbd2, test_ir, test_ir3;
+libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4), depth2rgb(1920, 1080 + 2, 4); // check here (https://github.com/OpenKinect/libfreenect2/issues/337) and here (https://github.com/OpenKinect/libfreenect2/issues/464) why depth2rgb image should be bigger
+
+std::mutex mat_lock;
 
 void sigint_handler(int s)
 {
   protonect_shutdown = true;
 }
 
-bool protonect_paused = false;
-libfreenect2::Freenect2Device *devtopause;
-
-//Doing non-trivial things in signal handler is bad. If you want to pause,
-//do it in another thread.
-//Though libusb operations are generally thread safe, I cannot guarantee
-//everything above is thread safe when calling start()/stop() while
-//waitForNewFrame().
-void sigusr1_handler(int s)
+void get_latest_frames()
 {
-  if (devtopause == 0)
-    return;
-/// [pause]
-  if (protonect_paused)
-    devtopause->start();
-  else
-    devtopause->stop();
-  protonect_paused = !protonect_paused;
-/// [pause]
+    //! [loop start]
+    while(!protonect_shutdown)
+    {
+        listener.waitForNewFrame(frames);
+        libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+        libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+        libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+        //! [loop start]
+
+        mat_lock.lock();
+        cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data).copyTo(rgbmat);
+        cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irmat);
+        cv::Mat(depth->height, depth->width, CV_32FC1, depth->data).copyTo(depthmat);
+        
+        irmat.convertTo(test_ir, CV_8UC1, 255.0/4096.0);
+        cv::merge(std::vector<cv::Mat>(3, test_ir), test_ir3);
+        
+        //! [registration]
+        registration->apply(rgb, depth, &undistorted, &registered, true, &depth2rgb);
+        //! [registration]
+
+        cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data).copyTo(depthmatUndistorted);
+        cv::Mat(registered.height, registered.width, CV_8UC4, registered.data).copyTo(rgbd);
+        cv::Mat(depth2rgb.height, depth2rgb.width, CV_32FC1, depth2rgb.data).copyTo(rgbd2);
+        mat_lock.unlock();
+        
+
+    //! [loop end]
+        listener.release(frames);
+    }
+    //! [loop end]
+
 }
 
-//The following demostrates how to create a custom logger
-/// [logger]
-#include <fstream>
-#include <cstdlib>
-class MyFileLogger: public libfreenect2::Logger
+void get_detections(bool output_dets, bool output_image, bool input_image, std::string base_path)
 {
-private:
-  std::ofstream logfile_;
-public:
-  MyFileLogger(const char *filename)
-  {
-    if (filename)
-      logfile_.open(filename);
-    level_ = Debug;
-  }
-  bool good()
-  {
-    return logfile_.is_open() && logfile_.good();
-  }
-  virtual void log(Level level, const std::string &message)
-  {
-    logfile_ << "[" << libfreenect2::Logger::level2str(level) << "] " << message << std::endl;
-  }
-};
-/// [logger]
+    NanoDet detector = NanoDet("./nanodet.param", "./nanodet.bin", false);
+    int i = 0;
+    int img_multiple = 10;
+    while (!protonect_shutdown && i < img_multiple * 1000)
+    {
+        mat_lock.lock();
+        if (test_ir3.empty())
+        {
+            mat_lock.unlock();
+            continue;
+        }
+        if (input_image && i++ % img_multiple == 0)
+        {
+            i++;
+            cv::imwrite(base_path + "/" + std::to_string(i) + ".jpg", test_ir3);
+        }
 
-/// [main]
-/**
- * Main application entry point.
- *
- * Accepted argumemnts:
- * - cpu Perform depth processing with the CPU.
- * - gl  Perform depth processing with OpenGL.
- * - cl  Perform depth processing with OpenCL.
- * - <number> Serial number of the device to open.
- * - -noviewer Disable viewer window.
- */
+        auto results = detector.resize_detect_and_draw(test_ir3);
+        mat_lock.unlock();
+        std::vector<BoxInfo> dets = std::get<0>(results);
+        cv::Mat result_img = std::get<1>(results);
+        
+        if (output_dets)
+        {
+            std::string json = "[";
+            for (int i = 0; i < dets.size(); i++)
+            {
+                if (i != 0)
+                    json += ", ";
+                json += "{\"label\": " + std::to_string(dets[i].label) + ", \"score\": " + std::to_string(dets[i].score) + ", \"x1\": " + std::to_string(dets[i].x1) + ", \"y1\": " + std::to_string(dets[i].y1) + ", \"x2\": " + std::to_string(dets[i].x2) + ", \"y2\": " + std::to_string(dets[i].y2) + "}";
+            }
+            json += "]";
+            std::cout << json << std::endl;
+        }
+        
+        if (output_image)
+        {
+            cv::imwrite("../../images/18-01-pm/result.jpg", result_img);
+        }
+    }
+    std::cout << "Finished taking pictures!" << std::endl;
+}
+
+
 int main(int argc, char *argv[])
-/// [main]
 {
-  std::string program_path(argv[0]);
-  std::cerr << "Version: " << LIBFREENECT2_VERSION << std::endl;
-  std::cerr << "Environment variables: LOGFILE=<protonect.log>" << std::endl;
-  std::cerr << "Usage: " << program_path << " [-gpu=<id>] [gl | cl | clkde | cuda | cudakde | cpu] [<device serial>]" << std::endl;
-  std::cerr << "        [-noviewer] [-norgb | -nodepth] [-help] [-version]" << std::endl;
-  std::cerr << "        [-frames <number of frames to process>]" << std::endl;
-  std::cerr << "To pause and unpause: pkill -USR1 Protonect" << std::endl;
-  size_t executable_name_idx = program_path.rfind("Protonect");
+    std::cout << "Streaming from Kinect One sensor!" << std::endl;
+    protonect_shutdown = false;
 
-  std::string binpath = "/";
-
-  if(executable_name_idx != std::string::npos)
-  {
-    binpath = program_path.substr(0, executable_name_idx);
-  }
-
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-  // avoid flooing the very slow Windows console with debug messages
-  libfreenect2::setGlobalLogger(libfreenect2::createConsoleLogger(libfreenect2::Logger::Info));
-#else
-  // create a console logger with debug level (default is console logger with info level)
-/// [logging]
-  libfreenect2::setGlobalLogger(libfreenect2::createConsoleLogger(libfreenect2::Logger::Debug));
-/// [logging]
-#endif
-/// [file logging]
-  MyFileLogger *filelogger = new MyFileLogger(getenv("LOGFILE"));
-  if (filelogger->good())
-    libfreenect2::setGlobalLogger(filelogger);
-  else
-    delete filelogger;
-/// [file logging]
-
-/// [context]
-  libfreenect2::Freenect2 freenect2;
-  libfreenect2::Freenect2Device *dev = 0;
-  libfreenect2::PacketPipeline *pipeline = 0;
-/// [context]
-
-  std::string serial = "";
-
-  bool viewer_enabled = true;
-  bool enable_rgb = true;
-  bool enable_depth = true;
-  int deviceId = -1;
-  size_t framemax = -1;
-
-  for(int argI = 1; argI < argc; ++argI)
-  {
-    const std::string arg(argv[argI]);
-
-    if(arg == "-help" || arg == "--help" || arg == "-h" || arg == "-v" || arg == "--version" || arg == "-version")
+    bool output_dets = false;
+    bool output_image = false;
+    bool input_image = false;
+    std::string base_path = "";
+    
+    if (argc >= 5)
     {
-      // Just let the initial lines display at the beginning of main
-      return 0;
-    }
-    else if(arg.find("-gpu=") == 0)
-    {
-      if (pipeline)
-      {
-        std::cerr << "-gpu must be specified before pipeline argument" << std::endl;
-        return -1;
-      }
-      deviceId = atoi(argv[argI] + 5);
-    }
-    else if(arg == "cpu")
-    {
-      if(!pipeline)
-/// [pipeline]
-        pipeline = new libfreenect2::CpuPacketPipeline();
-/// [pipeline]
-    }
-    else if(arg == "gl")
-    {
-#ifdef LIBFREENECT2_WITH_OPENGL_SUPPORT
-      if(!pipeline)
-        pipeline = new libfreenect2::OpenGLPacketPipeline();
-#else
-      std::cout << "OpenGL pipeline is not supported!" << std::endl;
-#endif
-    }
-    else if(arg == "cl")
-    {
-#ifdef LIBFREENECT2_WITH_OPENCL_SUPPORT
-      if(!pipeline)
-        pipeline = new libfreenect2::OpenCLPacketPipeline(deviceId);
-#else
-      std::cout << "OpenCL pipeline is not supported!" << std::endl;
-#endif
-    }
-    else if(arg == "clkde")
-    {
-#ifdef LIBFREENECT2_WITH_OPENCL_SUPPORT
-      if(!pipeline)
-        pipeline = new libfreenect2::OpenCLKdePacketPipeline(deviceId);
-#else
-      std::cout << "OpenCL pipeline is not supported!" << std::endl;
-#endif
-    }
-    else if(arg == "cuda")
-    {
-#ifdef LIBFREENECT2_WITH_CUDA_SUPPORT
-      if(!pipeline)
-        pipeline = new libfreenect2::CudaPacketPipeline(deviceId);
-#else
-      std::cout << "CUDA pipeline is not supported!" << std::endl;
-#endif
-    }
-    else if(arg == "cudakde")
-    {
-#ifdef LIBFREENECT2_WITH_CUDA_SUPPORT
-      if(!pipeline)
-        pipeline = new libfreenect2::CudaKdePacketPipeline(deviceId);
-#else
-      std::cout << "CUDA pipeline is not supported!" << std::endl;
-#endif
-    }
-    else if(arg.find_first_not_of("0123456789") == std::string::npos) //check if parameter could be a serial number
-    {
-      serial = arg;
-    }
-    else if(arg == "-noviewer" || arg == "--noviewer")
-    {
-      viewer_enabled = false;
-    }
-    else if(arg == "-norgb" || arg == "--norgb")
-    {
-      enable_rgb = false;
-    }
-    else if(arg == "-nodepth" || arg == "--nodepth")
-    {
-      enable_depth = false;
-    }
-    else if(arg == "-frames")
-    {
-      ++argI;
-      framemax = strtol(argv[argI], NULL, 0);
-      if (framemax == 0) {
-        std::cerr << "invalid frame count '" << argv[argI] << "'" << std::endl;
-        return -1;
-      }
+        if (strcmp(argv[1], "1") == 0)
+            output_dets = true;
+        if (strcmp(argv[2], "1") == 0)
+            output_image = true;
+        if (strcmp(argv[3], "1") == 0)
+            input_image = true;
+        base_path = argv[4];
     }
     else
     {
-      std::cout << "Unknown argument: " << arg << std::endl;
+        std::cout << "Usage: ./Protonect dets (0|1) out (0|1) in (0|1) path/to/save" << std::endl;
+        return -1;
     }
-  }
+    
+    
+    
+    std::cout << "Output detections: " << output_dets << std::endl;
+    std::cout << "Output image: " << output_image << std::endl;
+    std::cout << "Input image: " << input_image << std::endl;
 
-  if (!enable_rgb && !enable_depth)
-  {
-    std::cerr << "Disabling both streams is not allowed!" << std::endl;
-    return -1;
-  }
 
-/// [discovery]
-  if(freenect2.enumerateDevices() == 0)
-  {
-    std::cout << "no device connected!" << std::endl;
-    return -1;
-  }
-
-  if (serial == "")
-  {
-    serial = freenect2.getDefaultDeviceSerialNumber();
-  }
-/// [discovery]
-
-  if(pipeline)
-  {
-/// [open]
-    dev = freenect2.openDevice(serial, pipeline);
-/// [open]
-  }
-  else
-  {
-    dev = freenect2.openDevice(serial);
-  }
-
-  if(dev == 0)
-  {
-    std::cout << "failure opening device!" << std::endl;
-    return -1;
-  }
-
-  devtopause = dev;
-
-  signal(SIGINT,sigint_handler);
-#ifdef SIGUSR1
-  signal(SIGUSR1, sigusr1_handler);
-#endif
-  protonect_shutdown = false;
-
-/// [listeners]
-  int types = 0;
-  if (enable_rgb)
-    types |= libfreenect2::Frame::Color;
-  if (enable_depth)
-    types |= libfreenect2::Frame::Ir | libfreenect2::Frame::Depth;
-  libfreenect2::SyncMultiFrameListener listener(types);
-  libfreenect2::FrameMap frames;
-
-  dev->setColorFrameListener(&listener);
-  dev->setIrAndDepthFrameListener(&listener);
-/// [listeners]
-
-/// [start]
-  if (enable_rgb && enable_depth)
-  {
-    if (!dev->start())
-      return -1;
-  }
-  else
-  {
-    if (!dev->startStreams(enable_rgb, enable_depth))
-      return -1;
-  }
-
-  std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
-  std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
-/// [start]
-
-/// [registration setup]
-  libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
-  libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
-/// [registration setup]
-
-  size_t framecount = 0;
-#ifdef EXAMPLES_WITH_OPENGL_SUPPORT
-  Viewer viewer;
-  if (viewer_enabled)
-    viewer.initialize();
-#else
-  viewer_enabled = false;
-#endif
-
-/// [loop start]
-  while(!protonect_shutdown && (framemax == (size_t)-1 || framecount < framemax))
-  {
-    if (!listener.waitForNewFrame(frames, 10*1000)) // 10 sconds
+    //! [discovery]
+    if(freenect2.enumerateDevices() == 0)
     {
-      std::cout << "timeout!" << std::endl;
-      return -1;
+        std::cout << "no device connected!" << std::endl;
+        return -1;
     }
-    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
-    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
-/// [loop start]
 
-    if (enable_rgb && enable_depth)
+    string serial = freenect2.getDefaultDeviceSerialNumber();
+
+    std::cout << "SERIAL: " << serial << std::endl;
+
+    if(pipeline)
     {
-/// [registration]
-      registration->apply(rgb, depth, &undistorted, &registered);
-/// [registration]
+        //! [open]
+        dev = freenect2.openDevice(serial, pipeline);
+        //! [open]
+    } else {
+        dev = freenect2.openDevice(serial);
     }
 
-    framecount++;
-    if (!viewer_enabled)
+    if(dev == 0)
     {
-      if (framecount % 100 == 0)
-        std::cout << "The viewer is turned off. Received " << framecount << " frames. Ctrl-C to stop." << std::endl;
-      listener.release(frames);
-      continue;
+        std::cout << "failure opening device!" << std::endl;
+        return -1;
     }
 
-#ifdef EXAMPLES_WITH_OPENGL_SUPPORT
-    if (enable_rgb)
+    signal(SIGINT, sigint_handler);
+
+
+    dev->setColorFrameListener(&listener);
+    dev->setIrAndDepthFrameListener(&listener);
+    //! [listeners]
+
+    //! [start]
+    dev->start();
+
+    std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+    std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+    //! [start]
+
+    //! [registration setup]
+    registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+    //! [registration setup]
+    //
+    std::thread frame_thread(get_latest_frames);
+    frame_thread.detach();
+    std::thread nanodet_thread(get_detections, output_dets, output_image, input_image, base_path);
+    nanodet_thread.detach();
+    
+    int i = 1;
+    while(!protonect_shutdown)
     {
-      viewer.addFrame("RGB", rgb);
-    }
-    if (enable_depth)
-    {
-      viewer.addFrame("ir", ir);
-      viewer.addFrame("depth", depth);
-    }
-    if (enable_rgb && enable_depth)
-    {
-      viewer.addFrame("registered", &registered);
-    }
+        cin>>i;
+        if (!i)
+            protonect_shutdown = true;
+    } 
 
-    protonect_shutdown = protonect_shutdown || viewer.render();
-#endif
+    //! [stop]
+    dev->stop();
+    dev->close();
+    //! [stop]
 
-/// [loop end]
-    listener.release(frames);
-    /** libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100)); */
-  }
-/// [loop end]
+    delete registration;
 
-  // TODO: restarting ir stream doesn't work!
-  // TODO: bad things will happen, if frame listeners are freed before dev->stop() :(
-/// [stop]
-  dev->stop();
-  dev->close();
-/// [stop]
-
-  delete registration;
-
-  return 0;
+    std::cout << "Streaming Ends!" << std::endl;
+    return 0;
 }
+
+
+// void display()
+// {
+//     std::vector<BoxInfo> dets;
+//     switch (arg)
+//     {
+//     case 1:
+//         cv::imshow("rgb", rgbmat);
+//         break;
+//     case 2:
+//         cv::imshow("ir", irmat / 4096.0f);
+//         break;
+//     case 3:
+//         cv::imshow("depth", depthmat / 4096.0f);
+//         break;
+//     case 4:
+//         cv::imshow("undistorted", depthmatUndistorted / 4096.0f);
+//         break;
+//     case 5:
+//         cv::imshow("registered", rgbd);
+//         break;
+//     case 6:
+//         cv::imshow("depth2RGB", rgbd2 / 4096.0f);
+//         break;
+//     case 7:
+//         cv::imshow("test_ir", test_ir3);
+//         break;
+//     case 8:
+//         
+//         break;
+//     case 9:
+//         break;
+//     }
+// }
+
+// //! [nanodet]
+// //! [nanodet]
---
+++ examples/nanodet.h
@@ -0,0 +1,170 @@
+//
+// Create by RangiLyu
+// 2020 / 10 / 2
+//
+
+#ifndef NANODET_H
+#define NANODET_H
+
+#include <opencv2/core/core.hpp>
+#include <net.h>
+
+const int color_list[80][3] =
+{
+    //{255 ,255 ,255}, //bg
+    {216 , 82 , 24},
+    {236 ,176 , 31},
+    {125 , 46 ,141},
+    {118 ,171 , 47},
+    { 76 ,189 ,237},
+    {238 , 19 , 46},
+    { 76 , 76 , 76},
+    {153 ,153 ,153},
+    {255 ,  0 ,  0},
+    {255 ,127 ,  0},
+    {190 ,190 ,  0},
+    {  0 ,255 ,  0},
+    {  0 ,  0 ,255},
+    {170 ,  0 ,255},
+    { 84 , 84 ,  0},
+    { 84 ,170 ,  0},
+    { 84 ,255 ,  0},
+    {170 , 84 ,  0},
+    {170 ,170 ,  0},
+    {170 ,255 ,  0},
+    {255 , 84 ,  0},
+    {255 ,170 ,  0},
+    {255 ,255 ,  0},
+    {  0 , 84 ,127},
+    {  0 ,170 ,127},
+    {  0 ,255 ,127},
+    { 84 ,  0 ,127},
+    { 84 , 84 ,127},
+    { 84 ,170 ,127},
+    { 84 ,255 ,127},
+    {170 ,  0 ,127},
+    {170 , 84 ,127},
+    {170 ,170 ,127},
+    {170 ,255 ,127},
+    {255 ,  0 ,127},
+    {255 , 84 ,127},
+    {255 ,170 ,127},
+    {255 ,255 ,127},
+    {  0 , 84 ,255},
+    {  0 ,170 ,255},
+    {  0 ,255 ,255},
+    { 84 ,  0 ,255},
+    { 84 , 84 ,255},
+    { 84 ,170 ,255},
+    { 84 ,255 ,255},
+    {170 ,  0 ,255},
+    {170 , 84 ,255},
+    {170 ,170 ,255},
+    {170 ,255 ,255},
+    {255 ,  0 ,255},
+    {255 , 84 ,255},
+    {255 ,170 ,255},
+    { 42 ,  0 ,  0},
+    { 84 ,  0 ,  0},
+    {127 ,  0 ,  0},
+    {170 ,  0 ,  0},
+    {212 ,  0 ,  0},
+    {255 ,  0 ,  0},
+    {  0 , 42 ,  0},
+    {  0 , 84 ,  0},
+    {  0 ,127 ,  0},
+    {  0 ,170 ,  0},
+    {  0 ,212 ,  0},
+    {  0 ,255 ,  0},
+    {  0 ,  0 , 42},
+    {  0 ,  0 , 84},
+    {  0 ,  0 ,127},
+    {  0 ,  0 ,170},
+    {  0 ,  0 ,212},
+    {  0 ,  0 ,255},
+    {  0 ,  0 ,  0},
+    { 36 , 36 , 36},
+    { 72 , 72 , 72},
+    {109 ,109 ,109},
+    {145 ,145 ,145},
+    {182 ,182 ,182},
+    {218 ,218 ,218},
+    {  0 ,113 ,188},
+    { 80 ,182 ,188},
+    {127 ,127 ,  0},
+};
+
+typedef struct HeadInfo
+{
+    std::string cls_layer;
+    std::string dis_layer;
+    int stride;
+};
+
+struct CenterPrior
+{
+    int x;
+    int y;
+    int stride;
+};
+
+typedef struct BoxInfo
+{
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float score;
+    int label;
+} BoxInfo;
+
+struct object_rect {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+
+cv::Mat draw_bboxes(const cv::Mat& bgr, const std::vector<BoxInfo>& bboxes, object_rect effect_roi);
+int resize_uniform(cv::Mat& src, cv::Mat& dst, cv::Size dst_size, object_rect& effect_area);
+
+class NanoDet
+{
+public:
+    NanoDet(const char* param, const char* bin, bool useGPU);
+
+    ~NanoDet();
+
+    static NanoDet* detector;
+    ncnn::Net* Net;
+    static bool hasGPU;
+    // modify these parameters to the same with your config if you want to use your own model
+    int input_size[2] = {416, 416}; // input height and width
+    int num_class = 80; // number of classes. 80 for COCO
+    int reg_max = 7; // `reg_max` set in the training config. Default: 7.
+    std::vector<int> strides = { 8, 16, 32, 64 }; // strides of the multi-level feature.
+
+    std::vector<BoxInfo> detect(cv::Mat image, float score_threshold, float nms_threshold);
+    std::tuple<std::vector<BoxInfo>, cv::Mat> resize_detect_and_draw(cv::Mat& image);
+
+
+    std::vector<std::string> labels{ "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+                                    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+                                    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+                                    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+                                    "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+                                    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+                                    "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                                    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+                                    "hair drier", "toothbrush" };
+private:
+    void preprocess(cv::Mat& image, ncnn::Mat& in);
+    void decode_infer(ncnn::Mat& feats, std::vector<CenterPrior>& center_priors, float threshold, std::vector<std::vector<BoxInfo>>& results);
+    BoxInfo disPred2Bbox(const float*& dfl_det, int label, float score, int x, int y, int stride);
+    static void nms(std::vector<BoxInfo>& result, float nms_threshold);
+
+};
+
+
+#endif //NANODET_H
